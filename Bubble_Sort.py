6fc01c65c321a20dd9709d86f4c9aa3f652980fe
sigma3.14159265 <birajdarshiwam@gmail.com>
2020-09-12 01:57:28 +0530

    Bubble_Sort

---
+++ Bubble_Sort.py
@@ -0,0 +1,17 @@
+print('Enter the all numbers at once but seperated by a space :\n ')
+Array=input().split(' ')
+def Bubble_Sort(Array):
+    for i in range(0,len(Array)-1):
+        for j in range(i+1,len(Array)):
+            key=Array[i]
+            if key>=Array[j]:
+                Array[i]=Array[j]
+                Array[j]=key
+    return Array
+            
+print('Sorted Array :')
+print(Bubble_Sort(Array))
+
+
+
+
