1767ca1b0cf0eea2281cc9daaafbfdb94626e527
sigma3.14159265 <birajdarshiwam@gmail.com>
2020-09-16 15:08:06 +0530

    Kadane's Algo

--- Kadane's_Algo.py
+++ Kadane's_Algo.py
@@ -1,9 +1,9 @@
 print('Enter array : ')
 A=input().split(' ')
 A=list(map(int,A))
-curr_sum=A[0]
-maximum=A[0]
 def Kadanes(A):
+    curr_sum=A[0]
+    maximum=A[0]
     for i in range(1,len(A)):
         curr_sum=max(curr_sum + A[i],A[i])
         maximum=max(maximum,curr_sum)
